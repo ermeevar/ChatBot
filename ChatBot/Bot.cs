198bd3f3fd4270354ddbdc7e5619090f8a1565ad
ermeeva_r <ermeeva.r@gmail.com>
2021-09-19 23:33:02 +0300

    First

---
+++ ChatBot/Bot.cs
@@ -0,0 +1,29 @@
+﻿namespace ChatBot
+{
+    public class Bot
+    {
+        delegate void StartGuide();
+        private delegate void EndGuide();
+
+        StartGuide startGuide;
+        EndGuide endGuide;
+
+        public Bot()
+        {
+            startGuide = HelloMessage;
+            startGuide += InformationMessage;
+            startGuide += ChoiceMessage;
+
+            
+        }
+
+        private void HelloMessage() => System.Console.WriteLine("Привет");
+        private void InformationMessage() => System.Console.WriteLine(@"Это блог про ""честные"" выборы");
+        private void ChoiceMessage() => System.Console.WriteLine("Что бы вы хотели узнать?");
+        private void GratitudeMessage() => System.Console.WriteLine(@"Спасибо за участие, ваш голос очень ""важен""");
+        private void ByeMessage() => System.Console.WriteLine("До новых встреч");
+
+        public void PushStartMessages() => startGuide?.Invoke();
+        public void PushEndMessages() => endGuide?.Invoke();
+    }
+}
---
+++ ChatBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ChatBot
+{
+    class Program
+    {
+        delegate T AnyDelegate<T, F>(F prm);
+
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+
+            #region Putin
+            //Bot putinBot = new Bot();
+
+            //putinBot.PushStartMessages();
+
+            //Thread.Sleep(5000);
+
+            //putinBot.PushEndMessages();
+            #endregion
+
+            AnyDelegate<bool, string> anyDelegate = IsStringEmpty;
+            
+        }
+
+        public static bool IsStringEmpty(string prm) => String.IsNullOrEmpty(prm);
+    }
+
+    class B : MulticastDelegate { }
+}
