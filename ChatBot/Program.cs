ea4c9c8cd78c019a065014b7fd8408c3ce789fd7
incognito <incognito@mail>
2021-09-24 19:27:38 +0300

    Added events examples

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -5,28 +5,19 @@ namespace ChatBot
 {
     class Program
     {
-        delegate T AnyDelegate<T, F>(F prm);
-
         static void Main(string[] args)
         {
             Bot bot = new Bot();
+            bot.dateStart += PrintStartDate;
+            bot.dateStart += PrintEnvironment;
 
-            #region Putin
-            //Bot putinBot = new Bot();
-
-            //putinBot.PushStartMessages();
-
-            //Thread.Sleep(5000);
-
-            //putinBot.PushEndMessages();
-            #endregion
-
-            AnyDelegate<bool, string> anyDelegate = IsStringEmpty;
-            
+            bot.StartDialog();
         }
 
-        public static bool IsStringEmpty(string prm) => String.IsNullOrEmpty(prm);
-    }
+        public static void PrintStartDate() =>
+            Console.WriteLine($"Дата запуска бота: {DateTime.Now}");
 
-    class B : MulticastDelegate { }
+        public static void PrintEnvironment() =>
+            Console.WriteLine($"Среда: {Environment.CurrentDirectory}");
+    }
 }
